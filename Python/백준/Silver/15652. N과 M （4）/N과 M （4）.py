a5ddb31a909212d8fcfcce0514ff73616bfd7f08
CodingPenguin <code.penguin.yoon@gmail.com>
2025-09-06 23:43:48 +0900

    [Silver III] Title: N과 M (4), Time: 72 ms, Memory: 34884 KB -BaekjoonHub

---
+++ Python/백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,29 @@
+"""
+1. 로직
+- 백트래킹
+
+2. 러닝 타임
+- N^M  < 10
+
+3. 변수
+res = []
+"""
+
+from collections import deque
+
+n, m = map(int, input().split())
+res = deque()
+
+def back(a):
+    s=a
+    if len(res) == m:
+        print(*res)
+        return
+    for i in range(s, n+1):
+
+        res.append(i)
+        back(i)
+        res.pop()
+
+
+back(1)
