4b57b3874fc405ad01253c09ce01309a92bdaee1
CodingPenguin <code.penguin.yoon@gmail.com>
2025-09-11 02:48:11 +0900

    [Bronze II] Title: 피보나치 수, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- PyPy3/백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ PyPy3/백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -2,11 +2,11 @@ n = int(input())
 re1 = 1
 re2 = 1
 re3 = 0
-re_list = [0,1,1,2,3]
-if n < 5:
+re_list = [0,1,1]
+if n <= 2:
     print(re_list[n])
 else:
-    for i in range(2,n):
+    for _ in range(n-2):
         re3 = re1 + re2
         re1 = re2
         re2 = re3
