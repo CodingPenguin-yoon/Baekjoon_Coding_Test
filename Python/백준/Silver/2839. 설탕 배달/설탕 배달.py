fef6c8834c3e096a71fd3e1d486aab843d26360f
Cho YunHo <code.penguin.yoon@gmail.com>
2025-11-25 00:08:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ Python/백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -2,16 +2,11 @@ N = int(input())
 
 C_5 = N // 5
 
-check = False
-
 for i in range( C_5 , -1 , -1 ):
     DIV5 = N - (5*i)
     DIV3 = DIV5 % 3
     if DIV3 == 0:
         print(i+(DIV5//3))
-        check = True
         break
-
-
-if not check:
+else:
     print(-1)
