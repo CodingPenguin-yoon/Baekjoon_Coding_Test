9b24765fce634b650de9e3b0e929c81695fd92c2
Cho YunHo <code.penguin.yoon@gmail.com>
2025-11-24 17:18:57 +0900

    [Silver V] Title: 나이순 정렬, Time: 2596 ms, Memory: 51552 KB -BaekjoonHub

--- Python/백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ Python/백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -8,6 +8,4 @@ for i in range(N):
     I = int(l[i][0])
     C_l[I].append(l[i])
 
-for bucket in C_l:
-    for person in bucket:
-        print(person[0], person[1])
+[print(*p) for bucket in C_l for p in bucket]
