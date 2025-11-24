f7335e50081d47ed57ae7c38ddcd0da179a33428
Cho YunHo <code.penguin.yoon@gmail.com>
2025-11-24 16:44:55 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+l = [list(map(int, input().split())) for _ in range(N)]
+C = 1
+C_l = []*N
+for i in range(N):
+    for j in range(N):
+        if l[i][0] < l[j][0] and l[i][1] < l[j][1]:
+            C += 1
+    C_l.append(C)
+    C = 1
+
+print(*C_l)
