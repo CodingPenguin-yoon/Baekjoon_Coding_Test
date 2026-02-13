cf81abff592cf0bc9fde38c51be4f19dbdf0b664
Cho YunHo <code.penguin.yoon@gmail.com>
2026-02-13 19:36:38 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2798. 블랙잭/블랙잭.py
+++ Python/백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,27 +1,17 @@
-"""
-1. 아이디어
+N, M = map(int, input().split())
 
-- 3장을 계속 더함
+N_list = list(map(int, input().split()))
+sum = 0
+ans = 0
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            sum = N_list[i] + N_list[j] + N_list[k]
+            if sum < M:
+                if ans < sum:
+                    ans = sum
+            elif sum == M:
+                ans = sum
+                break
 
-2. 시간 복잡도
-- N!
-
-3. 자료구조
-- cd[]
-
-"""
-
-n, m = map(int, input().split())
-cd = list(map(int,input().split()))
-black_answer = 0
-
-for i in range(n):
-    for j in range(i+1,n):
-        for k in range(j+1,n):
-            
-            current_sum = cd[i] + cd[j] + cd[k]
-            
-            if black_answer < current_sum <= m:
-                black_answer = current_sum
-
-print(black_answer)
+print(ans)
