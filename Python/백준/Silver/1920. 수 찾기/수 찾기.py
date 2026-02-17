ba08a9c0113de3b94e9d56b4e420bf9643b681e6
Cho YunHo <code.penguin.yoon@gmail.com>
2026-02-17 11:27:59 +0900

    [Silver IV] Title: 수 찾기, Time: 124 ms, Memory: 51712 KB -BaekjoonHub

--- Python/백준/Silver/1920. 수 찾기/수 찾기.py
+++ Python/백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,15 +1,44 @@
+# #첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다. 다음 줄에는 M(1 ≤ M ≤ 100,000)이 주어진다. 다음 줄에는 M개의 수들이 주어지는데, 이 수들이 A안에 존재하는지 알아내면 된다. 모든 정수의 범위는 -231 보다 크거나 같고 231보다 작다. 10,000,000,000
+#
+# N = int(input())
+#
+# num_array = list(map(int,input().split()))
+#
+# M = int(input())
+#
+# check_array = list(map(int,input().split()))
+#
+#
+# num_max = max(num_array)
+#
+# num_array_sort = [0]*(num_max+1)
+#
+# for i in num_array:
+#     num_array_sort[i] = 1
+#
+# for i in check_array:
+#     try:
+#         if num_array_sort[i] == 1:
+#             print(1)
+#         else:
+#             print(0)
+#     except:
+#         print(0)
+#         continue
+
+
 N = int(input())
-
-A = list(map(int,input().split()))
-
-A_set = set(A)
-
+#
+num_array = list(map(int,input().split()))
+#
 M = int(input())
+#
+check_array = list(map(int,input().split()))
 
-B = list(map(int,input().split()))
+num_set = set(num_array)
 
-for i in range(M):
-    if B[i] in A_set:
+for i in check_array:
+    if i in num_set:
         print(1)
     else:
         print(0)
