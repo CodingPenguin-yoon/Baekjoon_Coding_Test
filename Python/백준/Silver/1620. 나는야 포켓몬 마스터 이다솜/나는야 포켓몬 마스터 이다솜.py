a1751b9f6b7ffde9064f79ad4e47e9c8071c9ace
Cho YunHo <code.penguin.yoon@gmail.com>
2026-02-24 14:59:00 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 228 ms, Memory: 47720 KB -BaekjoonHub

--- Python/백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ Python/백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,14 +1,19 @@
+
+import sys
+
+input = sys.stdin.readline
+
 N, M = map(int,input().split())
 dic = {}
 N_list = [" "]
 for i in range(1,N+1):
-    Poket = input()
+    Poket = input().strip()
     dic[Poket] = i
     N_list.append(Poket)
 
 
 for _ in range(M):
-    P = input()
+    P = input().strip()
     if P.isdigit():
         print(N_list[int(P)])
     else:
