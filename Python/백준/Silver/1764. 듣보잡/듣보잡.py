b2f7613f0c35d74d1e6c1afbde6dfe14a8759235
Cho YunHo <code.penguin.yoon@gmail.com>
2026-02-24 15:16:47 +0900

    [Silver IV] Title: 듣보잡, Time: 68 ms, Memory: 38048 KB -BaekjoonHub

--- Python/백준/Silver/1764. 듣보잡/듣보잡.py
+++ Python/백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,23 +1,23 @@
+
 import sys
+
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
+N, M = map(int,input().split())
+list_N = []
+ans_list = []
+for i in range(1,N+1):
+    list_N.append(input().strip())
 
-# 1. 입력 받을 때부터 .strip()으로 깔끔하게 넣기
-member_array = set()
-for _ in range(N):
-    member_array.add(input().strip())
-
-answer_array = [] # 결과는 순서가 중요하니 리스트로!
+set_N = set(list_N)
 
 for _ in range(M):
-    mem = input().strip() # 비교할 때도 깔끔하게
-    if mem in member_array:
-        answer_array.append(mem)
+    P = input().strip()
+    if P in set_N:
+        ans_list.append(P)
 
-# 2. ★핵심★ 보통 문제는 '사전순 출력'을 원합니다. 정렬 필수!
-answer_array.sort()
+ans_list.sort()
 
-print(len(answer_array))
-for i in answer_array:
+print(len(ans_list))
+for i in ans_list:
     print(i)
