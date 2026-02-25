a33a7a4eb306262336b7482e79b2ccebcb73a489
Cho YunHo <code.penguin.yoon@gmail.com>
2026-02-25 11:20:55 +0900

    [Silver I] Title: 용돈 관리, Time: 2920 ms, Memory: 36264 KB -BaekjoonHub

---
+++ Python/백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -0,0 +1,30 @@
+N, M = map(int,input().split())
+N_array = []
+
+for _ in range(N):
+    N_array.append(int(input()))
+
+start = max(N_array)
+end = sum(N_array)
+mid = 0
+result = 0
+while start <= end:
+    cnt = 1
+    mid = (start + end)//2
+    current_money = mid
+
+    for i in N_array:
+        if current_money < i:
+            cnt += 1
+            current_money = mid
+        current_money -= i
+
+
+    if cnt > M:
+        start = mid + 1
+
+    else:
+        result = mid
+        end = mid - 1
+
+print(result)
