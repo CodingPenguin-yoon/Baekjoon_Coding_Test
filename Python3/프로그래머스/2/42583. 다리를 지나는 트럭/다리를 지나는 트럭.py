831cfe4765266c0eea79a7d82af24b867923fe39
Cho YunHo <code.penguin.yoon@gmail.com>
2026-03-03 17:54:11 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.80 ms, Memory: 9.25 MB -BaekjoonHub

--- Python3/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ Python3/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,23 +1,50 @@
+# from collections import deque
+
+# def solution(bridge_length, weight, truck_weights):
+#     truck_que = deque(truck_weights)
+#     cnt = 0
+#     bridge = deque([0] * bridge_length)
+#     truck_weight = 0
+    
+    
+#     while bridge:
+#         cnt += 1
+#         exit_t = bridge.popleft()
+#         truck_weight -= exit_t
+        
+#         if truck_que:
+#             if truck_que[0] + truck_weight <= weight:
+#                 truck_buf = truck_que.popleft()
+#                 bridge.append(truck_buf)
+#                 truck_weight += truck_buf
+#             else:
+#                 bridge.append(0)
+
+#     return cnt
+
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    truck_que = deque(truck_weights)
-    cnt = 0
-    bridge = deque([0] * bridge_length)
-    truck_weight = 0
+    truck_weights = deque(truck_weights)
+    bridge = deque()
     
+    current_weight = 0
+    time = 0
     
-    while bridge:
-        cnt += 1
-        exit_t = bridge.popleft()
-        truck_weight -= exit_t
+    while truck_weights or bridge:
+        time += 1
         
-        if truck_que:
-            if truck_que[0] + truck_weight <= weight:
-                truck_buf = truck_que.popleft()
-                bridge.append(truck_buf)
-                truck_weight += truck_buf
+        if bridge and bridge[0][1] == time:
+            w, _ = bridge.popleft()
+            current_weight -= w
+            
+        if truck_weights:
+            if current_weight + truck_weights[0] <= weight:
+                w = truck_weights.popleft()
+                current_weight += w
+                bridge.append((w, time + bridge_length))
             else:
-                bridge.append(0)
-
-    return cnt
+                if bridge:
+                    time = bridge[0][1] - 1
+                    
+    return time
